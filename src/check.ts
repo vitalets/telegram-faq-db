27d7ab48376413c0b9c841df81e25d3a002437f3
Vitaliy Potapov <noginsk@rambler.ru>
2022-11-03 21:12:09 +0400

    get chat msg

--- src/check.ts
+++ src/check.ts
@@ -6,6 +6,8 @@ const client = new TgClient();
 try {
   await client.login();
   console.log('logged in');
+  const messages = await client.getChatHistory(-1001669191797, 10);
+  console.log(messages);
 } finally {
   await client.close();
 }
--- src/index.ts
+++ src/index.ts
@@ -11,14 +11,19 @@ import { AuthCode } from './authCode.js'
 
 const tdlibPath = process.platform === 'linux'
   ? getTdjson()
-   // ? './libtdjson/libtdjson.so.1.8.7'
+  // ? './libtdjson/libtdjson.so.1.8.7'
   : '../td/tdlib/lib/libtdjson.dylib'; // local development
 
-// const dbPath = process.platform === 'linux'
-//   ? '/tmp'
-//   : './tmp'; // local development
+const dbPath = './tmp';
 
-const dbPath = './tmp'
+// chatid: -1001669191797
+
+export type MyMessage = {
+  id: number;
+  to: number;
+  text: string;
+  toText?: string;
+}
 
 export class TgClient {
   protected client: Client;
@@ -45,7 +50,42 @@ export class TgClient {
   async close() {
     await this.client.close();
   }
+
+  async getChatHistory(chatId: number, count: number) {
+    // load chats first to avoid "Chat not found" error
+    const chats = await this.client.invoke({
+        _: 'getChats',
+        chat_list: { _: 'chatListMain' },
+        limit: 4000
+      });
+    console.log(chats);
+
+    const res: MyMessage[] = [];
+    let fromMessageId = 0;
+    // todo: what if channel does not have count messages?
+    while (res.length < count) {
+      console.log(`Loading messages from: ${fromMessageId}`);
+      const { messages } = await this.client.invoke({
+        _: 'getChatHistory',
+        chat_id: chatId,
+        from_message_id: fromMessageId,
+        offset: 0,
+        limit: 100,
+      });
+      console.log(`Loaded: ${messages.length}`);
+      messages
+        .filter(m => m?.content._ === 'messageText')
+        .forEach(m => m && res.push({
+          id: m.id,
+          to: m.reply_to_message_id,
+          text: (m.content as messageText).text.text,
+        }));
+      fromMessageId = messages.slice(-1)[ 0 ]!.id;
+    }
+    return res.slice(0, count);
+  }
 }
+
 
 
 
@@ -94,12 +134,7 @@ export class TgClient {
 
 
 /*
-type MyMessage = {
-  id: number;
-  to: number;
-  text: string;
-  toText?: string;
-}
+
 
 async function loadMessages(count: number) {
   const res: MyMessage[] = [];
